b649bf2acd6c6ac5cbaea0f0374153cf9e58c381
adrianobarbosa1 <adriano.barbosa100@gmail.com>
2022-04-17 14:55:13 -0300

    passando function por props

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,13 +1,54 @@
 import { GlobalStyle } from "./styles/global";
 import { Header } from './components/Header'
 import { Dashboard } from "./components/Dashboard";
+import { createServer } from "miragejs"
+import Modal from 'react-modal'
+import { useState } from "react";
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+
+        }
+      ]
+    })
+  }
+})
+
+Modal.setAppElement('#root');
 
 function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
   return (
     <>
       <GlobalStyle />
-      <Header />
-      <Dashboard/>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
+      <Dashboard />
     </>
 
   );
